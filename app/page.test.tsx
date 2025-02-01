e00936cb32d5df4b0ea1b9d28067b411a6b2cdd7
salahmander <salahabdo@hotmail.co.uk>
2025-02-01 16:20:02 +0000

    feat: basic test

---
+++ app/page.test.tsx
@@ -0,0 +1,18 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import Page from "./page";
+
+describe("Page", () => {
+  it("renders a heading", () => {
+    render(<Page />);
+
+    const heading = screen.getByRole("heading", { level: 1 });
+
+    expect(heading).toBeInTheDocument();
+  });
+});
+
+it("renders homepage unchanged", () => {
+  const { container } = render(<Page />);
+  expect(container).toMatchSnapshot();
+});
